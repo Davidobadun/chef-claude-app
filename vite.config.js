eab96299168ca53f397667917383fa358a8027c9
David Obadun <davidobadunofficial@outlook.com>
2025-02-08 20:37:22 -0500

    Updated file description

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/chef-claude-app",
+  server: {
+    host: '0.0.0.0', // Allow access from any IP
+    port: 3000, // Use a default port
+  },
+  build: {
+    outDir: 'build', // Folder for the production build
+  }
 })
